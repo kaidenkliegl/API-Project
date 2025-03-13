a35e6e55eeb608fd1480209af8968fbaaa7a0f00
Leo Angulo <anguloleo@gmail.com>
2025-03-12 19:40:38 -0700

    firstname lastname working

--- backend/db/seeders/20250307095046-demo-user.js
+++ backend/db/seeders/20250307095046-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'John',
+        lastName: "Doe",
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Deb',
+        lastName: "Ross",
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Ben',
+        lastName: "Ten",
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
