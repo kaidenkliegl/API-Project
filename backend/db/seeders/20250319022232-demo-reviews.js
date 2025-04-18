50a398815b1940c8eea431b6a154a1bd600fc375
Kaiden Kliegl <132409071+kaidenkliegl@users.noreply.github.com>
2025-04-17 18:41:25 -0700

    adjusted some seeder files

--- backend/db/seeders/20250319022232-demo-reviews.js
+++ backend/db/seeders/20250319022232-demo-reviews.js
@@ -2,6 +2,11 @@
 
 const { Review } = "../models"
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+ options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
--- backend/db/seeders/20250321005826-demo-review-images.js
+++ backend/db/seeders/20250321005826-demo-review-images.js
@@ -2,6 +2,11 @@
 
 const { ReviewsImage } = "../models"
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+ options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
