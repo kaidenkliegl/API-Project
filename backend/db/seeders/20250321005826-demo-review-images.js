132d52f5f94b0c4a626b01bd823ed5c62e951cc1
Kaiden Kliegl <132409071+kaidenkliegl@users.noreply.github.com>
2025-04-17 18:48:23 -0700

    with the imports

--- backend/db/seeders/20250321005826-demo-review-images.js
+++ backend/db/seeders/20250321005826-demo-review-images.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await ReviewsImage.bulkCreate([
+    await ReviewImage.bulkCreate([
       {
         reviewId: 1,  // Make sure this ID exists in the Reviews table
         url: "https://example.com/review-image1.jpg",
